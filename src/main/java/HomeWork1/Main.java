a788a73a2f6ee268b0c92b2b991a9babb55fee7c
Aleksandr <101212310+alekskrd@users.noreply.github.com>
2023-01-26 21:51:24 +0300

    Add files via upload
    Домашняя задание 2

--- src/main/java/HomeWork1/Main.java
+++ src/main/java/HomeWork1/Main.java
@@ -1,72 +1,64 @@
-package HomeWork1;
+package HomeWork2;
 
-import java.util.Random;
+import java.io.*;
 import java.util.Scanner;
 
-/* 1. Задать одномерный массив и найти в нем минимальный и максимальный элементы */
+/*1. Напишите метод, который принимает на вход строку (String) и
+определяет является ли строка палиндромом (возвращает boolean значение).*/
 public class Main {
     public static void main(String[] args) {
-        // hw1();
-        // hw2();
-         hw3();
+        hw1();
+        write();
     }
 
     public static void hw1() {
-        int[] arr = {5, 5, 4, 8, 5};
-        int min = arr[0];
-
-        for (int j : arr) {
-            if (j < min) {
-                min = j;
-            }
-        }
-        System.out.println(min);
-    }
-
-/* 2. Написать метод, который определяет, является ли введенный пользователем год високосным, и возвращает
-в консоль boolean (високосный - true, не високосный - false). Каждый 4-й год является високосным,
- кроме каждого 100-го, при этом каждый 400-й – високосный.*/
-
-    public static void hw2() {
         Scanner scan = new Scanner(System.in);
-        var number = Integer.parseInt(scan.nextLine());
+        System.out.println("Введите строку: ");
+        String string = scan.next();
 
-        if (number % 100 != 0 & number % 4 == 0 || number % 400 == 0) {
-            System.out.print("true");
-        } else {
-            System.out.print("false");
+        string = string.replaceAll("[^A-Za-zА-Яа-я0-9]", "");
+        if (string.equalsIgnoreCase((new StringBuilder(string))
+                .reverse()
+                .toString()))
+            System.out.println("Палиндром! " + string);
+        else
+            System.out.println("Не палиндром! :( " + string);
+    }
+
+/*2. Напишите метод, который составит строку, состоящую из 100 повторений слова TEST
+и метод, который запишет эту строку в простой текстовый файл, обработайте исключения.*/
+
+    public static StringBuilder test(String str) {
+//        var s = "TEST".repeat(100);
+//        System.out.println(s);
+
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < 100; i++) {
+            //String str = "TEST";
+            result.append(str);
         }
+        System.out.println(result);
+        return result;
+//        return s;
     }
 
+        public static void write () {
+            {
+                File file = new File("file.txt");
+                String text = String.valueOf(test("TEST"));
 
+                try (FileWriter fw = new FileWriter(file);
+                     BufferedWriter bf = new BufferedWriter(fw);
+                     PrintWriter out = new PrintWriter(bf)) {
+                    out.print(text);
 
-/*3. Дан массив nums = [3,2,2,3] и число val = 3.
-Если в массиве есть числа, равные заданному, нужно перенести эти элементы в конец массива.
-Таким образом, первые несколько (или все) элементов массива должны быть отличны от заданного,
- а остальные - равны ему.*/
+                    System.out.println("Successfully written data to the file");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
 
-    public static void Arr(int[] arr, int val) {
-        int right, left;
-        left = 0;
-        for (right = 0; right < arr.length; right++)
-            if (arr[right] != val) arr[left++] = arr[right];
-        for (right = left; right < arr.length; right++) arr[right] = val;
-    }
-
-
-    public static void hw3() {
-        int[] arr = new int[10];
-        Random random = new Random();
-
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = random.nextInt(4);
         }
 
-        for (int x : arr) System.out.print(x + " ");
-        System.out.println();
-        Arr(arr, 3);
-        for (int x : arr) System.out.print(x + " ");
-        System.out.println();
-    }
-}
 
+    }
